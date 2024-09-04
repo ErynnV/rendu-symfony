11810e2ce94508a3615ef6a19989339e46a1c9b1
Erynn Vandre <vandreerynn@gmail.com>
2024-09-04 18:18:15 +0200

    Delete option on recipe list

--- src/Form/RecipeDeleteType.php
+++ src/Form/RecipeDeleteType.php
@@ -3,8 +3,17 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class RecipeDeleteType extends AbstractType
 {
-    
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('id', HiddenType::class)
+            ->add('delete', SubmitType::class)
+        ;
+    }
 }
